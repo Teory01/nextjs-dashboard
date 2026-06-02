3888fede7c1f1b9d6de8ec63652f9a30fafa5758
teory01 <youremail@byupathway.edu>
2026-06-02 20:17:17 +0100

    modified and added metadata

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -1,4 +1,5 @@
 import postgres from 'postgres';
+console.log('POSTGRES_URL:', process.env.POSTGRES_URL);
 import {
   CustomerField,
   CustomersTableType,
@@ -13,15 +14,12 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -52,9 +50,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
@@ -160,10 +155,11 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
-    return invoice[0];
+    console.log(invoice);
+    return invoice[0] ?? null; // 👈 return null if not found
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
+    console.error('Database Error:', error); // 👈 log but don't throw
+    return null;                             // 👈 let notFound() handle it
   }
 }
 
